235d8d97b519300bfded3086be4417a5d280500e
ysx-hidden <394460456@qq.com>
2021-12-12 21:54:11 +0800

    下拉菜单组件封装

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="dropdown" ref="dropdownRef">
+        <a class="btn btn-outline-light my-2 dropdown-toggle" href="#" @click.prevent="toggle">{{
+            title
+        }}</a>
+        <ul class="dropdown-menu" style="display: block; left: -68%" v-show="visible">
+            <li v-for="(item, index) in dropdownList" :key="index">
+                <a class="dropdown-item" :class="{ disabled: item.disabled }" href="#">{{
+                    item.title ? item.title : '下拉选项'
+                }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref, watch } from '@vue/runtime-core'
+import useClickOutside from '../hooks/useClickOutside'
+
+export interface DropdownItem {
+    title?: string
+    disabled?: boolean
+}
+
+export default defineComponent({
+    name: 'Dropdown',
+    props: {
+        title: {
+            type: String,
+            default: '下拉菜单'
+        },
+        dropdownList: {
+            type: Array as PropType<DropdownItem[]>,
+            default: () => []
+        }
+    },
+    setup(props) {
+        const visible = ref(false)
+        const dropdownRef = ref<null | HTMLElement>(null)
+        const isClickOutside = useClickOutside(dropdownRef)
+        const toggle = () => {
+            if (props.dropdownList.length) {
+                visible.value = !visible.value
+            }
+        }
+        watch(isClickOutside, (val) => {
+            if (visible.value && val) {
+                visible.value = false
+            }
+        })
+        return {
+            visible,
+            toggle,
+            dropdownRef,
+            isClickOutside
+        }
+    }
+})
+</script>
+
+<style>
+.dropdown-menu .disabled {
+    color: #6c757d;
+    pointer-events: none;
+    background-color: transparent;
+}
+</style>
